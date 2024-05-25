4885b3c18674d52fc00a224b90ba24997837e7a6
Rajneesh  Das <rajneeshd@versa-networks.com>
2024-05-26 04:45:43 +0530

    default pages set

---
+++ app/loading.jsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react'
+import { ClipLoader } from 'react-spinners'
+
+const loading = ({loading}) => {
+    const override = {
+        display: 'block',
+        margin: '100px auto'
+    }
+  return (
+    <ClipLoader
+        color={'#3b82f6'}
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+    />
+  )
+}
+
+export default loading
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import properties from '@/properties.json'
+import PropertyCard from '@/components/PropertyCard'
 
 const Properties = () => {
   return (
-    <div>Properties</div>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {properties.map((property) => 
+              <PropertyCard
+                key={property.id}
+                property={property}
+              />
+            )
+          }
+        </div>
+      </div>
+    </section>
   )
 }
 
